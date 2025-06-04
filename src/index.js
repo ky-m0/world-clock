8191cc6aa79411d37cef9cf911f7fafbf99f2e88
ky-m0 <114282955+ky-m0@users.noreply.github.com>
2025-06-04 12:21:17 +1000

    setup time and date for city 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-//alert("hello world");
+// New York
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+
+newYorkDateElement.innerHTML = newYorkTime.format("dddd, d MMMM YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
