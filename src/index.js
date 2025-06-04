a4275d49ac7c94662c74fc16de4b82490f62c4d3
ky-m0 <114282955+ky-m0@users.noreply.github.com>
2025-06-04 12:39:30 +1000

    added second time zone and refresh each second

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-// New York
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time");
-let newYorkTime = moment().tz("America/New_York");
+function updateTime() {
+  // New York
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
-newYorkDateElement.innerHTML = newYorkTime.format("dddd, d MMMM YYYY");
-newYorkTimeElement.innerHTML = newYorkTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd, d MMMM YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, d MMMM YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
